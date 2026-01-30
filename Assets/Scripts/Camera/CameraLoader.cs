2179b2e97c405e1e026f7375a51f66d3e03279e9
Owen Plett <owen.plett@gmail.com>
2026-01-30 09:03:07 -0700

    fixed camera transitions

--- Assets/Scripts/Camera/CameraLoader.cs
+++ Assets/Scripts/Camera/CameraLoader.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 public class CameraLoader : MonoBehaviour
 {
     [SerializeField] private CinemachineCamera cam;
+    private void Awake()
+    {
+        cam.Priority = 0;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
