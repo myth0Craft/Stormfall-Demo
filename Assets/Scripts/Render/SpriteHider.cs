36ddcd8d5018eedc52dd3d5350973cdd393b0250
Owen Plett <owen.plett@gmail.com>
2026-04-20 10:08:13 -0600

    added script to make sprites transparent when player is in collider

---
+++ Assets/Scripts/Render/SpriteHider.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SpriteHider : MonoBehaviour
+{
+    public List<SpriteRenderer> spritesToHide = new List<SpriteRenderer>();
+    public float fadeDuration = 0.5f;
+    public float transparency = 0.2f;
+    public bool foregroundSprites = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(HideSpritesCoroutine(1.0f, transparency));
+        }        
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(HideSpritesCoroutine(transparency, 1.0f));
+        }
+    }
+
+    private IEnumerator HideSpritesCoroutine(float startAlpha, float endAlpha)
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / fadeDuration);
+
+            foreach (SpriteRenderer sprite in spritesToHide)
+            {
+                if (sprite != null)
+                {
+                    sprite.color = Color.Lerp(new Color(sprite.color.r, sprite.color.g, sprite.color.b, startAlpha), new Color(sprite.color.r, sprite.color.g, sprite.color.b, endAlpha), t);
+                }
+            }
+            
+            if (foregroundSprites)
+            {
+                if (endAlpha < startAlpha)
+                {
+                    foreach (SpriteRenderer sprite in spritesToHide)
+                    {
+                        sprite.gameObject.layer = LayerMask.NameToLayer("Default");
+                    }
+
+                } else
+                {
+                    foreach (SpriteRenderer sprite in spritesToHide)
+                    {
+                        sprite.gameObject.layer = LayerMask.NameToLayer("Foreground");
+                    }
+                }
+                
+            }
+            
+            
+            yield return null;
+        }
+    }
+
+}
